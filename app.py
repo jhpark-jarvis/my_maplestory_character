57ef5387cb60f0ab107d26daa5456e87003f1471
re-brother <jaehyung_96@naver.com>
2026-04-19 17:53:03 +0900

    API 로그인 수정 진행

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from maple_app.data import *
 from maple_app import app
-from flask import render_template, request
+from flask import render_template, request, flash
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Handle login logic here
+        # Handle login logic here      
         api_key = request.form.get('api_key')
-        api_key_check(api_key)
-        pass
-    return render_template('login.html')
+        result = json.loads(api_key_check(api_key))
+        print("###############################")
+        #print(result)
+        print(type(result))
+        if result["result_code"] == 200:
+            # 로그인 성공 시 캐릭터 정보 페이지로 리디렉션
+            #return render_template('character_info.html', character_data=result["result_data"])
+            return result["result_data"]
+        else:
+            # 로그인 실패 시 에러 메시지 표시
+            flash(result["result_message"] + ": " + result["result_fail_reason"], 'error')
+            return render_template('login.html')
+    
+    else:
+        return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- maple_app/data.py
+++ maple_app/data.py
@@ -4,10 +4,13 @@ import requests, sys, bs4, json
 import pandas as pd
 
 def api_key_check(api_key):
+    target_url = 'https://open.api.nexon.com/maplestory/v1/character/list'
     result_code = 200
     result_message = "Success"
     result_fail_reason = ""
-    
+    result_data = ""
+
+    # 개발/테스트용 API 키 체크
     if "test_" in api_key:
         result_code = 400
         result_message = "개발/테스트용 API키 입니다."
@@ -15,66 +18,78 @@ def api_key_check(api_key):
         return_data = {
             "result_code": result_code,
             "result_message": result_message,
-            "result_fail_reason": result_fail_reason
+            "result_fail_reason": result_fail_reason,
+            "result_data": ""
         }
         
         json_result = json.dumps(return_data, ensure_ascii=False)
         print(json_result)
         return(json_result)
     
-
-target_url = 'https://open.api.nexon.com/maplestory/v1/character/list'
-api_key = open("api_key.txt", "r").read().strip()
-api_key = api_key.replace("LIVE_KEY=", "")  # Ensure the API key is correctly formatted
-result_code = 200
-result_message = "Success"
-result_fail_reason = ""
-
-curl_command = f"curl -X 'GET' \
-  '{target_url}' \
-  -H 'accept: application/json' \
-  -H 'x-nxopen-api-key: {api_key}'"
-  
-res = requests.get(target_url, headers={"accept": "application/json", "x-nxopen-api-key": api_key + ""})
-res_text = bs4.BeautifulSoup(res.text, "html.parser")
-
-try:
-    res_json = json.loads(res_text.text)
-except json.JSONDecodeError as e:
-    print(f"Error decoding JSON: {e}")
-    print(res_text.text)
-    result_code = 500
-    result_message = "Failed to decode JSON response"
-    result_fail_reason = "Invalid JSON format"
-    sys.exit(1)
-
-
-if (res.status_code != 200):
-    print(f"Error: {res.status_code}")
-    print(res_json)
-    result_code = res.status_code
-    result_message = "Error occurred while fetching data"
-    result_fail_reason = res.reason
-    sys.exit(1)
     
+    curl_command = f"curl -X 'GET' \
+    '{target_url}' \
+    -H 'accept: application/json' \
+    -H 'x-nxopen-api-key: {api_key}'"
+    
+    res = requests.get(target_url, headers={"accept": "application/json", "x-nxopen-api-key": api_key})
+    res_text = bs4.BeautifulSoup(res.text, "html.parser")
+
+    # 잘못된 API 키
+    try:
+        res_json = json.loads(res_text.text)
+        if res.status_code == 200:
+            return json.dumps({
+                "result_code": res.status_code,
+                "result_message": "API 키 로그인 성공",
+                "result_fail_reason": "",
+                "result_data": res_json
+            }, ensure_ascii=False)
+        else:
+            # %%
+            #print(res_json)
+            return json.dumps({
+                "result_code": res.status_code,
+                "result_message": "API 키 로그인 실패",
+                "result_fail_reason": res_json['error']['message'],
+                "result_data": ""
+            }, ensure_ascii=False)
+        
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        print(res_text.text)
+        result_code = 500
+        result_message = "Failed to decode JSON response"
+        result_fail_reason = "Invalid JSON format"
+        return_data = {
+            "result_code": result_code,
+            "result_message": result_message,
+            "result_fail_reason": result_fail_reason
+        }
+        json_result = json.dumps(return_data, ensure_ascii=False)
+        print(json_result)
+        return(json_result)
+
+"""
 account_ids = []
 for account in res_json['account_list']:
     account_ids.append(account['account_id'])
     
-print(account_ids)
+#print(account_ids)
 
 df_characters = pd.DataFrame(res_json['account_list'][0]['character_list'])
-print(df_characters)
-print(df_characters['world_name'] == '크로아')
+#print(df_characters)
+#print(df_characters['world_name'] == '크로아')
 df_characters = df_characters[df_characters['world_name'] == '크로아']
 
 df_duplicated = df_characters[df_characters.duplicated(subset=['character_class'], keep=False)]
-print(df_duplicated)
+#print(df_duplicated)
 
 df_headers = df_characters.columns.tolist()
 # header_template = "<tr>" + "".join([f"<th>{header}</th>" for header in df_headers]) + "</tr>"
 # df_characters_template = df_characters.to_html(index=False, header=True)
-# return_table_template = f"""<table id=\"myTable\" border=\"1\">
+# return_table_template = f"""
+# <table id=\"myTable\" border=\"1\">
 #     <thead>
 #         {header_template}
 #     </thead>
@@ -90,3 +105,5 @@ df_headers = df_characters.columns.tolist()
 
 # print(return_table_template)
 
+
+# %%
